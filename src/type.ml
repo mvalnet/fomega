1ff90e62c969547d782707a607d391c59b2cc402
mvalnet <milla.valnet@ens.fr>
2022-01-26 00:26:33 +0100

    Implementing equality functions

--- src/type.ml
+++ src/type.ml
@@ -55,7 +55,57 @@ let _lazy =
 
 let norm t1 = t1
 let eq_typ t1 t2 = compare t1 t2 = 0
-let diff_typ t1 t2 = None (* fix me *)
 
+let eq_binder b1 b2 =
+  match b1, b2 with
+  | Tlam, Tlam | Tall, Tall | Texi, Texi -> true
+  | _ -> false
 
+let eq_cvar v1 v2 =
+  v1.name = v2.name && v1.id = v2.id
+
+let eq_prim p1 p2 =
+  match p1, p2 with
+  | Tint, Tint | Tbool, Tbool
+  | Tstring, Tstring | Tunit, Tunit -> true
+  | _ -> false
+
+let rec recurse_if_equal h1 h2 q1 q2 =
+  match diff_typ h1 h2 with
+  | None -> diff_typ q1 q2
+  | Some(_) as l -> l
+
+and diff_typ t1 t2 = 
+  match t1, t2 with
+  | Tvar(v1), Tvar(v2) ->
+    if eq_cvar v1 v2 then None
+    else Some(t1,t2)
+  
+  | Tprim(p1), Tprim(p2) -> 
+    if eq_prim p1 p2 then None
+    else Some(t1, t2)
+
+  | Tapp(f1, a1), Tapp(f2, a2)
+  | Tarr(f1, a1), Tarr(f2, a2) ->
+    recurse_if_equal f1 f2 a1 a2
+  
+  | Tprod([]), Tprod([]) | Trcd([]), Trcd([]) -> None
+  | Tprod([]), _ | _, Tprod([])
+  | Trcd([]), _  | _, Trcd([]) -> Some(t1, t2)
+
+  | Tprod(h1::q1), Tprod(h2::q2) ->
+    recurse_if_equal h1 h2 (Tprod q1) (Tprod q2)
+
+  | Trcd((l1,h1)::q1), Trcd((l2,h2)::q2) ->
+    if l1 <> l2 then
+      Some(t1,t2)
+    else
+      recurse_if_equal h1 h2 (Trcd q1) (Trcd q2)
+
+  | Tbind(b1,x1,k1,body1), Tbind(b2,x2,k2,body2) ->
+    if eq_binder b1 b2 && eq_cvar x1 x2 && eq_kind k1 k2 then
+      diff_typ body1 body2
+    else Some(t1, t2)
+
+  | _ -> Some(t1, t2)
 
